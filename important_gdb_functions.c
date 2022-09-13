53bd579a1df12f3efdb350212de149ce1df8a6c4
Milos Kalicanin <milos.kalicanin@syrmia.com>
2022-09-13 13:57:39 +0200

    Deleted less important functions from "Functions" and added "get_debug_line_section and find_pc_sect_line" example

--- important_gdb_functions.c
+++ important_gdb_functions.c
@@ -1,28 +1,20 @@
-/* Fetch the result of the expression evaluation in a form of
-     a struct value, where TYPE, SUBOBJ_TYPE and SUBOBJ_OFFSET
-     describe the source level representation of that result.
-     AS_LVAL defines if the fetched struct value is expected to
-     be a value or a location description.  */
-  value *fetch_result (struct type *type, struct type *subobj_type,
-		       LONGEST subobj_offset, bool as_lval);
-		       
+/* The engine for the expression evaluator.  Using the context in this
+   object, evaluate the expression between OP_PTR and OP_END.  
 
-/* The exported interface to dwarf2_evaluate_loc_desc_full; it always
-   passes 0 as the byte_offset.  */
-
-struct value *
-dwarf2_evaluate_loc_desc (struct type *type, struct frame_info *frame,
-			  const gdb_byte *data, size_t size,
-			  dwarf2_per_cu_data *per_cu,
-			  dwarf2_per_objfile *per_objfile, bool as_lval)
+   Called by evaluate and dwarf_expr_context::eval functions.
+   Has roughly 900 lines of code.    */ 
+void
+dwarf_expr_context::execute_stack_op (const gdb_byte *op_ptr,
+				      const gdb_byte *op_end)
 
 
 /* Evaluate a location description, starting at DATA and with length
    SIZE, to find the current location of variable of TYPE in the
    context of FRAME.  If SUBOBJ_TYPE is non-NULL, return instead the
    location of the subobject of type SUBOBJ_TYPE at byte offset
-   SUBOBJ_BYTE_OFFSET within the variable of type TYPE.  */
-
+   SUBOBJ_BYTE_OFFSET within the variable of type TYPE.  
+    
+   Called by function dwarf2_evaluate_loc_desc   */
 static struct value *
 dwarf2_evaluate_loc_desc_full (struct type *type, struct frame_info *frame,
 			       const gdb_byte *data, size_t size,
@@ -32,49 +24,18 @@ dwarf2_evaluate_loc_desc_full (struct type *type, struct frame_info *frame,
 			       LONGEST subobj_byte_offset,
 			       bool as_lval)
 
-/* Evaluate the expression at ADDR (LEN bytes long) in a given PER_CU
-     and FRAME context.
 
-     AS_LVAL defines if the returned struct value is expected to be a
-     value (false) or a location description (true).
-
-     TYPE, SUBOBJ_TYPE and SUBOBJ_OFFSET describe the expected struct
-     value representation of the evaluation result.
-
-     The ADDR_INFO property can be specified to override the range of
-     memory addresses with the passed in buffer.  */
-  value *evaluate (const gdb_byte *addr, size_t len, bool as_lval,
-		   dwarf2_per_cu_data *per_cu, frame_info *frame,
-		   const struct property_addr_info *addr_info = nullptr,
-		   struct type *type = nullptr,
-		   struct type *subobj_type = nullptr,
-		   LONGEST subobj_offset = 0);
+/* Fetch the result of the expression evaluation in a form of
+   a struct value, where TYPE, SUBOBJ_TYPE and SUBOBJ_OFFSET
+   describe the source level representation of that result.
+   AS_LVAL defines if the fetched struct value is expected to
+   be a value or a location description.  */
+value *fetch_result (struct type *type, struct type *subobj_type,
+		             LONGEST subobj_offset, bool as_lval);
 
 
-/* Evaluate the expression at ADDR (LEN bytes long).  */
-
-void
-dwarf_expr_context::eval (const gdb_byte *addr, size_t len)
-
-
-/* The engine for the expression evaluator.  Using the context in this
-   object, evaluate the expression between OP_PTR and OP_END.  */ 
-900 LINES OF CODE !!
-
-void
-dwarf_expr_context::execute_stack_op (const gdb_byte *op_ptr,
-				      const gdb_byte *op_end)
-
-
-/* Compute the DWARF CFA for a frame.  */
+/* Compute the DWARF CFA (Canonical Frame Address) for a frame.  */
 CORE_ADDR dwarf2_frame_cfa (struct frame_info *this_frame);
-
-// CFA - Canonical Frame Address
-
-
-/* Return the reason why we can't unwind past this frame.  */
-
-enum unwind_stop_reason get_frame_unwind_stop_reason (struct frame_info *);
 
 
 /* Return a "struct frame_info" corresponding to the frame that called
@@ -83,6 +44,7 @@ enum unwind_stop_reason get_frame_unwind_stop_reason (struct frame_info *);
    Unlike get_prev_frame, this function always tries to unwind the
    frame.  */
 extern struct frame_info *get_prev_frame_always (struct frame_info *);
+
 
 /* Return the per-frame unique identifer.  Can be used to relocate a
    frame after a frame cache flush (and other similar operations).  If
@@ -107,8 +69,6 @@ extern struct frame_id get_frame_id (struct frame_info *fi);
 
 /* Compute the frame's uniq ID that can be used to, later, re-find the
    frame.  */
-
 static void
 compute_frame_id (struct frame_info *fi)
-
 
